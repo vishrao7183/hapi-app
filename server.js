2c876398be9cf0f51cdb098b71cf7302be5b49d8
vishrao7183 <vishrao7183@gmail.com>
2020-03-16 17:25:39 +0530

    On branch master

---
+++ server.js
@@ -0,0 +1,29 @@
+require('./dbconnection');
+const Hapi = require ('hapi');
+const bplugin = require('./pulgin/baseRoute')
+var Routes = require('./routes/userRoutes')
+const auth = require('./pulgin/auth')
+const Plugin = require('./pulgin/sawagger')
+
+const init =async ()=>{
+
+const server = new Hapi.Server({
+    port:8000,
+    host : 'localhost'
+});
+await server.route(Routes());
+
+
+await server.register([Plugin,bplugin,auth])
+
+
+await server.start();
+
+ console.log('sever started at 8000');
+};
+// process.on('unhandledRejection',(err)=>{
+//     console.log(err);
+//     process.exit(1);
+// })
+
+init();
